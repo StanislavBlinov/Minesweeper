0791ce5602cf9c6752b0df14c0c23a5820abbae2
Stas Blinov <stanislav.blinov@gameloft.com>
2020-12-23 18:40:42 +0200

    * add buttons to mainMenu

--- sources/layout/mainMenu.py
+++ sources/layout/mainMenu.py
@@ -5,31 +5,26 @@ from pygame_gui import UIManager
 from manager.constants import Options
 from pygame_gui.elements import UIWindow
 from pygame_gui.elements import UIButton
+from pygame_gui.elements import UIHorizontalSlider
 
 class MainMenu(UIWindow):
     def __init__(self, rect, ui_manager):
         super().__init__(rect, ui_manager,
                          window_display_title='Scale',
                          object_id='#main_menu',
-                         resizable=False)
+                         resizable=True)
 
         self.options = Options()
-        self.startBtn = UIButton(pygame.Rect((int(self.options.resolution[0] / 2),
-                                                 int(self.options.resolution[1] * 0.90)),
-                                                (100, 40)),
-                                    '',
-                                    self.ui_manager,
-                                    tool_tip_text="<font face=fira_code color=normal_text size=2>"
-                                                  "<b><u>Test Tool Tip</u></b>"
-                                                  "<br><br>"
-                                                  "A little <i>test</i> of the "
-                                                  "<font color=#FFFFFF><b>tool tip</b></font>"
-                                                  " functionality."
-                                                  "<br><br>"
-                                                  "Unleash the Kraken!"
-                                                  "</font>",
-                                    object_id='#startBtn')
-
-    def update(self, time_delta):
-        super().update(time_delta)
-        self.startBtn.update(time_delta)
+        ui_window = self
+        button_layout_rect = pygame.Rect(30, 20, 100, 40)
+        self.disable_toggle = UIButton(relative_rect=button_layout_rect,
+                                       text='Disable',
+                                       manager=ui_manager,
+                                       object_id='#disable_button',
+                                       container=ui_window)
+        button_layout_rect1 = pygame.Rect(30, 80, 100, 40)
+        self.hide_toggle = UIButton(relative_rect=button_layout_rect1,
+                                    text='Hide',
+                                    manager=self.ui_manager,
+                                    object_id='#hide_button',
+                                    container=ui_window)
--- sources/manager/application.py
+++ sources/manager/application.py
@@ -3,12 +3,16 @@ import pygame_gui
 
 from manager.constants import Options
 from pygame_gui import UIManager
+from pygame_gui.elements import UIButton
 from layout.mainMenu import MainMenu
 
 class App():
     def __init__(self):
         pygame.init()
-        pygame.display.set_caption("Options UI")
+        pygame.display.set_caption("MineSweaper")
+
+        self.iconImage = pygame.image.load('data/images/mine.png')
+        pygame.display.set_icon(self.iconImage)
 
         self.options = Options()
         if self.options.fullscreen:
@@ -20,14 +24,17 @@ class App():
         self.background_surface = None
 
         self.ui_manager = UIManager(self.options.resolution, 'data/theme/mineSweaper_theme.json')
-        self.recreate_ui()
+
         self.clock = pygame.time.Clock()
         self.running = True
 
         '''
             Create game Window
         '''
-        self.mainMenu = MainMenu(pygame.Rect((0, 0), self.options.resolution), self.ui_manager)
+        self.mainMenu = None
+
+
+        self.recreate_ui()
 
     def process_events(self):
         for event in pygame.event.get():
@@ -44,22 +51,21 @@ class App():
         self.background_surface = pygame.Surface(self.options.resolution)
         self.background_surface.fill(self.ui_manager.get_theme().get_colour('dark_bg'))
 
+        self.mainMenu = MainMenu(pygame.Rect((25, 25), (self.options.resolution[0] - 250, self.options.resolution[1] - 50)), self.ui_manager)
+
     def run(self):
 
         while self.running:
             time_delta = self.clock.tick() / 1000.0
 
-            self.process_events()
             # check for input
+            self.process_events()
 
+            # respond to input
             self.ui_manager.update(time_delta)
-
-
 
             # draw graphics
             self.window_surface.blit(self.background_surface, (0, 0))
             self.ui_manager.draw_ui(self.window_surface)
-
-            self.mainMenu.update(time_delta)
 
             pygame.display.update()
